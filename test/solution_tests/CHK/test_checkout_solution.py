52a18ca71bee655fe43957e300bb700b4bf188ac
sbx_user1051 <sbx_user1051@169.254.62.73>
2025-04-15 21:00:13 +0000

    Tue Apr 15 21:00:13 UTC 2025

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -5,4 +5,6 @@ class TestCheckout:
     def test_checkout(self):
         assert CheckoutSolution().checkout("AAABB") == 175
         assert CheckoutSolution().checkout("AABCD") == 165
+        assert CheckoutSolution().checkout("AAAA") == 180
+
 
