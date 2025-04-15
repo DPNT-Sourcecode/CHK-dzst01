a55c0e72ca2e2ac00a99563dbd24921b62aaa949
sbx_user1051 <sbx_user1051@169.254.62.73>
2025-04-15 21:55:31 +0000

    Tue Apr 15 21:55:31 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,66 +2,71 @@ from collections import Counter
 
 
 class CheckoutSolution:
-	def __init__(self):
-		# sku: (saleQty, salePrice)
-		self.salePrices = {
-			"A": [(5, 200), (3, 130), (1, 50)],
-			"B": [(2, 45), (1, 30)],
-			"C": [(1, 20)],
-			"D": [(1, 15)],
-			"E": [(1, 40)],
-			"F": [(1, 10)],
-			"G": [(1, 20)],
-			"H": [(5, 45), (10,80), (1, 10)],  
-			"I": [(1, 35)],
-			"J": [(1, 60)],  
-			"K": [(1, 80)],    | 2K for 150             |
-			"L": [(1, 90)],
-			"M": [(1, 15)], 
-			"N": [(1, 40)],    | 3N get one M free      |
-			"O": [(1, 10)],
-			"P": [(5, 200), (1, 50)],    |              |
-			"Q": [(3Q for 80)(1, 30)],    | 3Q for 80              |
-			"R": [(1, 50)],    | 3R get one Q free      |
-			"S": [(1, 30)],  
-			"T": [(1, 20)],  
-			"U": [(1, 40)],    | 3U get one U free      |
-			"V": [(1, 50)],    | 2V for 90, 3V for 130  |
-			"W": [(1, 20)],
-			"X": [(1, 90)],
-			"Y": [(1, 10)],
-			"Z": [(1, 50)],
-		}
+    def __init__(self):
+        # sku: (saleQty, salePrice)
+        self.salePrices = {
+            "A": [(5, 200), (3, 130), (1, 50)],
+            "B": [(2, 45), (1, 30)],
+            "C": [(1, 20)],
+            "D": [(1, 15)],
+            "E": [(1, 40)],
+            "F": [(1, 10)],
+            "G": [(1, 20)],
+            "H": [(10, 80), (5, 45), (1, 10)],
+            "I": [(1, 35)],
+            "J": [(1, 60)],
+            "K": [(2, 150), (1, 80)],
+            "L": [(1, 90)],
+            "M": [(1, 15)],
+            "N": [(1, 40)],
+            "O": [(1, 10)],
+            "P": [(5, 200), (1, 50)],
+            "Q": [(3, 80), (1, 30)],
+            "R": [(1, 50)],
+            "S": [(1, 30)],
+            "T": [(1, 20)],
+            "U": [(1, 40)],
+            "V": [(3, 130)(2, 90)(1, 50)],
+            "W": [(1, 20)],
+            "X": [(1, 90)],
+            "Y": [(1, 10)],
+            "Z": [(1, 50)],
+        }
 
-		self.freeItems = {
-			"E": [("B", 2)],
-			"F": [("F", 3)],
-		}
+        self.freeItems = {
+            "E": [("B", 2)],
+            "F": [("F", 3)],
+            "F": [("F", 3)],
+            "N": [("M", 3)],
+            "R": [("Q", 3)],
+            "U": [("U", 4)],
+        }
 
-	# skus = unicode string
-	def checkout(self, skus):
-		# If not a string
-		if not isinstance(skus, str):
-			return -1
+    # skus = unicode string
+    def checkout(self, skus):
+        # If not a string
+        if not isinstance(skus, str):
+            return -1
 
-		# Counts all SKUs in the skus string
-		counts = Counter(skus)
+        # Counts all SKUs in the skus string
+        counts = Counter(skus)
 
-		try:
-			for sku, freeItemsList in self.freeItems.items():
-				for freeSku, saleQty in freeItemsList:
-					counts[freeSku] -= counts[sku] // saleQty
+        try:
+            for sku, freeItemsList in self.freeItems.items():
+                for freeSku, saleQty in freeItemsList:
+                    counts[freeSku] -= counts[sku] // saleQty
 
-			total = 0
-			for sku, count in counts.items():
-				for saleQty, salePrice in self.salePrices[sku]:
-					if count <= 0:
-						break
-					total += (count // saleQty) * salePrice
-					count %= saleQty
+            total = 0
+            for sku, count in counts.items():
+                for saleQty, salePrice in self.salePrices[sku]:
+                    if count <= 0:
+                        break
+                    total += (count // saleQty) * salePrice
+                    count %= saleQty
 
-		except KeyError:
-			# If skus not found in prices return -1
-			return -1
+        except KeyError:
+            # If skus not found in prices return -1
+            return -1
 
-		return total
+        return total
+
