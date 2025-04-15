cd11c9ee91b2d3e30b7c67f594ac30dac85872ae
sbx_user1051 <sbx_user1051@169.254.62.73>
2025-04-15 20:29:03 +0000

    Tue Apr 15 20:29:03 UTC 2025

---
+++ test/solution_tests/HLO/test_sum_solution.py
@@ -0,0 +1,10 @@
+from solutions.SUM.sum_solution import SumSolution
+
+
+class TestSum:
+    def test_sum(self):
+        assert SumSolution().compute(1, 2) == 3
+        assert SumSolution().compute(2, 2) == 4
+        assert SumSolution().compute(100, 100) == 200
+        assert SumSolution().compute(48, 2) == 50
+        assert SumSolution().compute(1, 99) == 100
