cba0c93aafec2f256cb68e72391d84af488f9894
sbx_user1051 <sbx_user1051@169.254.62.73>
2025-04-15 21:52:31 +0000

    Tue Apr 15 21:52:31 UTC 2025

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -18,5 +18,3 @@ class TestCheckout:
         assert CheckoutSolution().checkout("FFFFFF") == 40
         assert CheckoutSolution().checkout("hello") == -1
         assert CheckoutSolution().checkout(2234) == -1
-
-
