087e9525444e34fdc8d5599b25b01feef2b9085a
sbx_user1051 <sbx_user1051@169.254.62.73>
2025-04-15 20:43:18 +0000

    Tue Apr 15 20:43:18 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,11 @@
+from collections import Counter
+
 
 class CheckoutSolution:
-
     # skus = unicode string
     def checkout(self, skus):
-        raise NotImplementedError()
+        counts = Counter(skus)
+        total = 0
+B
+        return total
+
